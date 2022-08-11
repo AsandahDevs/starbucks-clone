36ec02d83da5106708b4f1b8a7e76d85373c0a2b
Asanda <noelmajola@gmail.com>
2022-08-11 18:20:32 +0200

    Import react-router-dom components for routing and import page components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SHARED_LAYOUT from "./pages/SHARED_LAYOUT";
+import Homepage from "./pages/Homepage";
+import Coffees from "./pages/Coffees";
+import Delivers from "./pages/Delivers";
+import Rewards from "./pages/Rewards";
+import ERROR_PAGE from "./pages/ERROR_PAGE";
+
 function App() {
-  return <h1>Starbucks website clone</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SHARED_LAYOUT />}>
+          <Route index element={<Homepage />} />
+          <Route path="coffees" element={<Coffees />} />
+          <Route path="delivers" element={<Delivers />} />
+          <Route path="rewards" element={<Rewards />} />
+          <Route path="*" element={<ERROR_PAGE />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
