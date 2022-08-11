1a0c88d32361305655115d2f004ed36e8bad4612
Asanda <noelmajola@gmail.com>
2022-08-11 22:27:54 +0200

    Add redirect link to homepage

--- src/pages/ERROR_PAGE.js
+++ src/pages/ERROR_PAGE.js
@@ -1,10 +1,18 @@
+import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
+
 const ERROR_PAGE = () => {
   return (
     <>
       <Container>
-        <h1>404</h1>
+        <h1>
+          <strong>404</strong>
+        </h1>
         <p>Page not found !</p>
+        <Link to="/">
+          <Button variant="outline-success">Back to homepage</Button>
+        </Link>
       </Container>
     </>
   );
