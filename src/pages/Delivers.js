c4364e86a5bd7738f7cf6eb04882b126d40a0b78
Asanda <noelmajola@gmail.com>
2022-08-16 14:24:54 +0200

    Add main and side content

--- src/pages/Delivers.js
+++ src/pages/Delivers.js
@@ -3,7 +3,21 @@ const Delivers = () => {
   return (
     <>
       <Container>
-        <h1>Delivers page</h1>
+        <main>
+          <h1>Main content</h1>
+        </main>
+        <aside className="backgroundImageForDeliversPage">
+          <p
+            className="backgroundTextForHomePage"
+            style={{
+              fontSize: "2.5rem",
+              fontWeight: "bold",
+              color: "transparent",
+            }}
+          >
+            Lorem text with a bunch of gibberish attached to it.
+          </p>
+        </aside>
       </Container>
     </>
   );
