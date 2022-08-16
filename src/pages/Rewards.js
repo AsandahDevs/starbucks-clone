e698ce45a2c46792ddeb880b66cd8c2bc739a564
Asanda <noelmajola@gmail.com>
2022-08-16 23:59:54 +0200

    Add placeholder text for main content and assign css class to aside tag to add background image

--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -3,7 +3,22 @@ const Rewards = () => {
   return (
     <>
       <Container>
-        <h1>Rewards page</h1>
+        <main>
+          <h1>Main content</h1>
+        </main>
+        <aside className="backgroundImageForRewardsPage">
+          {" "}
+          <p
+            className="backgroundTextForHomePage"
+            style={{
+              fontSize: "2.5rem",
+              fontWeight: "bold",
+              color: "transparent",
+            }}
+          >
+            Lorem text with a bunch of gibberish attached to it.
+          </p>
+        </aside>
       </Container>
     </>
   );
