6d7e508e7b493e173f14c2a75a8423e42cc840bb
Asanda <noelmajola@gmail.com>
2022-08-16 17:06:30 +0200

    Remove container component , apply margin styling for contents and padding for button component

--- src/pages/ERROR_PAGE.js
+++ src/pages/ERROR_PAGE.js
@@ -1,20 +1,19 @@
 import Button from "react-bootstrap/esm/Button";
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/esm/Container";
 
 const ERROR_PAGE = () => {
   return (
-    <>
-      <Container>
-        <h1>
-          <strong>404</strong>
-        </h1>
-        <p>Page not found !</p>
-        <Link to="/">
-          <Button variant="outline-success">Back to homepage</Button>
-        </Link>
-      </Container>
-    </>
+    <div style={{ marginLeft: "30rem", marginTop: "10rem" }}>
+      <h1>
+        <strong>404</strong>
+      </h1>
+      <p>Page not found !</p>
+      <Link to="/">
+        <Button variant="outline-success" style={{ padding: "0rem 1rem" }}>
+          Back to homepage
+        </Button>
+      </Link>
+    </div>
   );
 };
 
